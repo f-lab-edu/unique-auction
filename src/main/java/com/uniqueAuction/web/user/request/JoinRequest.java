39e1a0a54776db05de53002dca4ee5187efe97b1
superkkj <gabunot@gmail.com>
2022-09-13 23:38:28 +0900

    refactory : 에러부분 변경

--- src/main/java/com/uniqueAuction/web/user/request/JoinRequest.java
+++ src/main/java/com/uniqueAuction/web/user/request/JoinRequest.java
@@ -1,34 +1,53 @@
 package com.uniqueAuction.web.user.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 import com.uniqueAuction.domain.user.entity.Role;
 import com.uniqueAuction.domain.user.entity.User;
-import lombok.Builder;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class JoinRequest {
     private Long userId;
+    private Boolean isAdmin;
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,16}$", message = "비밀번호는 최소 8글자, 최대 16자 이며 영문 대소문자 1개, 숫자 1개가 각각 포함된 형태이어야 합니다.")
     private String password;
+
+    @NotBlank(message = "유저명은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[A-Za-z0-9]{4,12}$", message = "유저명 형식에 맞지 않습니다.")
     private String username;
+
+    @NotBlank(message = "휴대폰은 필수 입력 값입니다.")
+    @Pattern(regexp = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$", message = "휴대폰 번호 형식에 맞지 않습니다.")
     private String phone;
 
-    public User toUser() {
+    public JoinRequest() {
+        this.isAdmin = false;
+    }
+
+    public User toEntity(JoinRequest joinRequest) {
+
+        Role requestRole = Role.CUSTOMER;
+
+        if (joinRequest.getIsAdmin()) {
+            requestRole = Role.ADMIN;
+        }
+
         return User.builder()
-                .email(email)
-                .username(username)
-                .password(password)
-                .role(Role.CUSTOMER)
+                .email(joinRequest.getEmail())
+                .username(joinRequest.getUsername())
+                .encodedPassword(joinRequest.getPassword())
+                .phone(joinRequest.getPhone())
+                .role(requestRole)
                 .build();
     }
 
-    @Builder
-    public JoinRequest(String email, String password, String username, String phone) {
-        this.email = email;
-        this.password = password;
-        this.username = username;
-        this.phone = phone;
-    }
 }
