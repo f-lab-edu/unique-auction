838f775923480d4c01f0a174510523ed69ef2f4f
superkkj <gabunot@gmail.com>
2022-10-01 00:15:47 +0900

    Feature : 어노테이션 통합 기존 어노테이션 삭제.

--- src/main/java/com/uniqueAuction/web/user/request/JoinRequest.java
+++ src/main/java/com/uniqueAuction/web/user/request/JoinRequest.java
@@ -2,52 +2,47 @@ package com.uniqueAuction.web.user.request;
 
 import com.uniqueAuction.domain.user.entity.Role;
 import com.uniqueAuction.domain.user.entity.User;
-import com.utils.annotation.Email;
-import com.utils.annotation.Password;
-import com.utils.annotation.Phone;
-import com.utils.annotation.Username;
-
+import com.utils.annotation.RegExp;
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import static com.utils.RegExpCode.*;
 
 @Getter
 public class JoinRequest {
-	private Long userId;
-	private Boolean isAdmin;
+    private Long userId;
+    private Boolean isAdmin;
 
-	@Email
-	private String email;
+    @RegExp(regExpCode = EMAIL)
+    private String email;
 
-	@Password
-	private String password;
+    @RegExp(regExpCode = PASSWORD)
+    private String password;
 
-	@Username
-	private String username;
+    @RegExp(regExpCode = USERNAME)
+    private String username;
 
-	@Phone
-	private String phone;
+    @RegExp(regExpCode = PHONE)
+    private String phone;
 
-	public JoinRequest() {
-		this.isAdmin = false;
-	}
+    public JoinRequest() {
+        this.isAdmin = false;
+    }
 
-	public User toEntity(JoinRequest joinRequest) {
+    public User toEntity(JoinRequest joinRequest) {
 
-		Role requestRole = Role.CUSTOMER;
+        Role requestRole = Role.CUSTOMER;
 
-		if (joinRequest.getIsAdmin()) {
-			requestRole = Role.ADMIN;
-		}
+        if (joinRequest.getIsAdmin()) {
+            requestRole = Role.ADMIN;
+        }
 
-		return User.builder()
-			.email(joinRequest.getEmail())
-			.username(joinRequest.getUsername())
-			.encodedPassword(joinRequest.getPassword())
-			.phone(joinRequest.getPhone())
-			.role(requestRole)
-			.build();
-	}
+        return User.builder()
+                .email(joinRequest.getEmail())
+                .username(joinRequest.getUsername())
+                .encodedPassword(joinRequest.getPassword())
+                .phone(joinRequest.getPhone())
+                .role(requestRole)
+                .build();
+    }
 
 }
