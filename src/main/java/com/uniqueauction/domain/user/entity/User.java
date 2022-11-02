72c55d3e215650f77948398611befb3fe6bf533f
superkkj <gabunot@gmail.com>
2022-11-02 15:20:57 +0900

    fix : 유저 서비스 로직 버그 수정 및 유저 엔티티 어노테이션 (role)추가.

--- src/main/java/com/uniqueauction/domain/user/entity/User.java
+++ src/main/java/com/uniqueauction/domain/user/entity/User.java
@@ -1,6 +1,8 @@
 package com.uniqueauction.domain.user.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,6 +13,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ *  Enum 타입 일경우
+ *  @Enumerated(EnumType.STRING) 선언하지 않으면 enum필드 순서가 들어가는 인트값이 들어간다.
+ *  원하는 값을 넣어주기위해 설정을 넣어준다.
+ */
 @Getter
 @Entity
 @Builder
@@ -20,6 +27,8 @@ public class User {
 	private String email;
 	private String username;
 	private String phone;
+
+	@Enumerated(EnumType.STRING)
 	private Role role;
 
 	@Setter
