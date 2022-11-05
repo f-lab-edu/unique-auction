29fe8bfd363828a845b60821f58f57a9900a6420
vymr1000 <nuseehr@naver.com>
2022-11-06 01:59:12 +0900

    feature: Base Entity 추가

--- src/main/java/com/uniqueauction/UniqueAuctionApplication.java
+++ src/main/java/com/uniqueauction/UniqueAuctionApplication.java
@@ -1,13 +1,11 @@
 package com.uniqueauction;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UniqueAuctionApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/uniqueauction/domain/product/entity/Product.java
+++ src/main/java/com/uniqueauction/domain/product/entity/Product.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.uniqueauction.domain.base.BaseEntity;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +15,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Product {
+public class Product extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "product_id")
