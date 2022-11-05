cf58dd2103d71f6a716a463c52f6536ba93bbf5f
vymr1000 <nuseehr@naver.com>
2022-11-06 00:50:17 +0900

    fix: test fail 수정

--- src/test/java/com/uniqueauction/TestContainerBase.java
+++ src/test/java/com/uniqueauction/TestContainerBase.java
@@ -1,13 +1,12 @@
 package com.uniqueauction;
 
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
