a86cadba7e39dec8b2916420b763d9530c4e0112
superkkj <gabunot@gmail.com>
2022-11-07 01:05:16 +0900

    fix: 코드포멧수정

--- src/main/java/com/uniqueauction/UniqueAuctionApplication.java
+++ src/main/java/com/uniqueauction/UniqueAuctionApplication.java
@@ -2,16 +2,14 @@ package com.uniqueauction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
 @SpringBootApplication
 @EnableJpaAuditing
 public class UniqueAuctionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UniqueAuctionApplication.class, args);
 	}
-
 }
