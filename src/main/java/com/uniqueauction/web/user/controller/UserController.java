8502a37cf8f89afaeae04f3464b008b318174bec
superkkj <gabunot@gmail.com>
2022-11-11 15:28:23 +0900

    feature: 로그인 checker 추가 및 수정. 로그인한 아이디외에 다른 아이디로 수정이나 다른           요청이 들어오면 권한이 없는 예외를 던진다.

--- src/main/java/com/uniqueauction/web/user/controller/UserController.java
+++ src/main/java/com/uniqueauction/web/user/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.uniqueauction.web.user.controller;
 
+import static com.uniqueauction.domain.user.entity.Role.*;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.uniqueauction.domain.aop.LoginCheck;
 import com.uniqueauction.domain.user.service.UserService;
 import com.uniqueauction.web.response.CommonResponse;
 import com.uniqueauction.web.user.request.JoinRequest;
@@ -29,10 +33,11 @@ public class UserController {
 		return CommonResponse.success();
 	}
 
+	@LoginCheck(type = CUSTOMER)
 	@PatchMapping("/users/{id}")
 	@ResponseStatus(HttpStatus.OK)
-	public CommonResponse updateUser(@RequestBody @Validated UpdateUserRequest updateUserRequest,
-		BindingResult result) {
+	public CommonResponse updateUser(@PathVariable("id") String id,
+		@RequestBody @Validated UpdateUserRequest updateUserRequest, BindingResult result) {
 		userService.update(updateUserRequest);
 		return CommonResponse.success();
 	}
