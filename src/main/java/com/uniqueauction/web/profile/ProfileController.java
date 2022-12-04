eae058d90182e7491e46e328908c5e578c82aad7
superkkj <gabunot@gmail.com>
2022-12-05 02:50:35 +0900

    yrdy

--- src/main/java/com/uniqueauction/web/profile/ProfileController.java
+++ src/main/java/com/uniqueauction/web/profile/ProfileController.java
@@ -1,6 +1,7 @@
 package com.uniqueauction.web.profile;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +16,23 @@ public class ProfileController {
 	private final Environment env;
 
 	//profile 조회
-	@GetMapping(value = "/profile")
-	public String getProfile() {
-		return Arrays.stream(env.getActiveProfiles())
-			.findFirst()
-			.orElse("");
+	//  @GetMapping(value = "/profile")
+	// public String getProfile() {
+	// 	return Arrays.stream(env.getActiveProfiles())
+	// 		.findFirst()
+	// 		.orElse("");
+	// }
+
+	@GetMapping("/profile")
+	public String profile() {
+		List<String> profiles = Arrays.asList(env.getActiveProfiles());
+		List<String> realProfiles = Arrays.asList("real1", "real2");
+
+		String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+		return profiles.stream()
+			.filter(realProfiles::contains)
+			.findAny()
+			.orElse(defaultProfile);
 	}
 }
