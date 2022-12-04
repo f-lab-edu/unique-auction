bdb1d6ac40bc2e4031b62632973123f4ab185a44
superkkj <gabunot@gmail.com>
2022-12-04 23:22:13 +0900

    스크립트 폴더이동 및 그래들

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,37 +1,68 @@
-ACCESSKEY="3llRJaaOdNenSwt7HruV"
+BUILD_PATH=$(ls build/libs/*-SNAPSHOT.jar)
+JAR_NAME=$(ls $BUILD_PATH | grep 'uniqueauction' | tail -n 1 | xargs -0 -n 1 basename )
+echo "> build 파일명: $JAR_NAME"
 
-SECRETKEY="TGFs7Jjsul9376tTIsypkUNN1KvuxE26jQHmhvp0"
-
-project=4873
-
-stage=5632
-
-scenario=5893
-
-METHOD="POST"
+echo "> build 파일 복사"
+DEPLOY_PATH=/home/project/unique-auction-1/jar/
+sudo cp $BUILD_PATH $DEPLOY_PATH
 
 
+RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-function makeSignature() {
+echo ">$RESPONSE_CODE response code"
 
-        nl=$'\\n'
-
-        SIG="$4"' '"$5"${nl}"$3"${nl}"$1"
-
-        SIGNITURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $2 -binary|openssl enc -base64)
-
-}
-
-
-
-TIMESTAMP=$(echo $(($(date +%s%N)/1000000)))
-
-URI="/api/v1/project/$project/stage/$stage/scenario/$scenario/deploy"
+# 아무것도 구동중이지  않을 때 우리가 프로젝트에서 만든 api로 조회하면 당연히 오류가 난다. 이를 방지!
+if [ ${RESPONSE_CODE} -ge 400 ]
+then
+        echo "> There is no running app lets set real2"
+        CURRENT_PROFILE=real2
+else
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+fi
 
 
+echo ">$CURRENT_PROFILE current profile"
 
-makeSignature $ACCESSKEY $SECRETKEY $TIMESTAMP $METHOD $URI
+if [ $CURRENT_PROFILE == real1 ]
+then
+  IDLE_PROFILE=real2
+  IDLE_PORT=8082
+elif [ $CURRENT_PROFILE == real2 ]
+then
+  IDLE_PROFILE=real1
+  IDLE_PORT=8081
+else
+  echo "> 일치하는 Profile이 없습니다. Profile: $CURRENT_PROFILE"
+  echo "> real1을 할당합니다. IDLE_PROFILE: real1"
+  IDLE_PROFILE=real1
+  IDLE_PORT=8081
+fi
+
+IDLE_APPLICATION=$IDLE_PROFILE-uniqueauction.jar
+
+sudo ln -Tfs $DEPLOY_PATH$JAR_NAME $DEPLOY_PATH$IDLE_PROFILE-uniqueauction.jar
 
 
+echo "> $IDLE_PROFILE 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(pgrep -f $IDLE_APPLICATION)
 
-curl -X $METHOD https://sourcedeploy.apigw.ntruss.com$URI -H "accept: application/json" -H "x-ncp-region_code: KR" -H "x-ncp-iam-access-key: $ACCESSKEY" -H "x-ncp-apigw-timestamp: $TIMESTAMP" -H "x-ncp-apigw-signature-v2: $SIGNITURE"
+if [ -z $IDLE_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $IDLE_PID"
+  sudo kill -15 $IDLE_PID
+  sleep 10
+fi
+
+echo "> $IDLE_PROFILE 배포"
+echo "> Change Directory to $DEPLOY_PATH "
+cd $DEPLOY_PATH
+echo "> $IDLE_APPLICATION Deploying "
+
+nohup java -jar $IDLE_APPLICATION --spring.profiles.default=$IDLE_PROFILE &
+
+
+echo "> Profile Switching"
+sleep 10
+./home/project/unique-auction/scripts/switch.sh
