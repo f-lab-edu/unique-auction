bec9f5aaa8d2eb9d0b069ef916866bee18ffc99c
vymr1000 <nuseehr@naver.com>
2023-02-11 15:31:30 +0900

    refactor: 피드백 반영, 조회 케이스 흐름에 따라 로직 처리

--- src/main/java/com/uniqueauction/domain/trade/service/TradeService.java
+++ src/main/java/com/uniqueauction/domain/trade/service/TradeService.java
@@ -2,6 +2,8 @@ package com.uniqueauction.domain.trade.service;
 
 import static com.uniqueauction.exception.ErrorCode.*;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,27 +36,34 @@ public class TradeService {
 		Product product = productRepository.findById(tradeRequest.getProductId())
 			.orElseThrow(() -> new CommonException(NOT_FOUND_PRODUCT));
 
-		/* product와 product size, 구매 입찰 중인 trade 조회 - 조회되는 것이 있으면 업데이트 하기 위함 */
-		Trade trade = tradeRepository.findByProductIdAndProductSizeAndTradeStatusAndPriceLessThanEqual(product.getId(),
-				tradeRequest.getProductSize(),
-				TradeStatus.SALE_PROGRESS, tradeRequest.getPrice())
-			.orElse(tradeRepository.findByPublisherIdAndProductIdAndProductSizeAndTradeStatus(buyer.getId(),
-					product.getId(),
-					tradeRequest.getProductSize(),
-					TradeStatus.PURCHASE_PROGRESS)
-				.orElse(tradeRequest.convertForBuyer(product.getId())));
+		/* product와 product size, 판매 입찰 중인 trade 조회 - 조회되는 것이 있으면 거래 체결 하기 위함 */
+		Optional<Trade> trade = tradeRepository.findByProductIdAndProductSizeAndTradeStatusAndPriceLessThanEqual(
+			product.getId(),
+			tradeRequest.getProductSize(),
+			TradeStatus.SALE_PROGRESS, tradeRequest.getPrice());
 
-		/* 판매 입찰이 있는 경우 거래 체결을 위한 update, 기존 요청을 찾았을 경우 기존 요청 update  */
-		if (trade.getTradeStatus() == TradeStatus.SALE_PROGRESS) {
-			trade.updateTradeByBuyer(tradeRequest.getUserId(), tradeRequest.getPrice());
-		} else if (trade.getTradeStatus() == TradeStatus.PURCHASE_PROGRESS) {
-			trade.updateTrade(tradeRequest.getPrice(), tradeRequest.getShippingAddress());
+		/* 구매자가 찾는 판매 요청 내역이 있을 경우 거래 체결 */
+		if (trade.isPresent()) {
+			trade.get().updateTradeByBuyer(tradeRequest.getUserId(), tradeRequest.getPrice());
+		} else {
+			/* 기존 요청한 구매 요청 있는지 조회 한다  */
+			trade = tradeRepository.findByPublisherIdAndProductIdAndProductSizeAndTradeStatus(buyer.getId(),
+				product.getId(),
+				tradeRequest.getProductSize(),
+				TradeStatus.PURCHASE_PROGRESS);
+
+			/* 기존 요청 내역 존재시 업데이트, 없으면 신규 생성 */
+			if (trade.isPresent()) {
+				trade.get().updateTrade(tradeRequest.getPrice(), tradeRequest.getShippingAddress());
+			} else {
+				trade = Optional.ofNullable(tradeRequest.convertForSeller(product.getId()));
+			}
 		}
 
 		/*
 		판매자에게 message 전송
 		 */
-		tradeRepository.save(trade);
+		trade.ifPresent(tradeRepository::save);
 	}
 
 	@Transactional
@@ -67,28 +76,33 @@ public class TradeService {
 		Product product = productRepository.findById(tradeRequest.getProductId())
 			.orElseThrow(() -> new CommonException(NOT_FOUND_PRODUCT));
 
-		/* product와 product size, 구매 입찰 중인 trade 조회 - 조회되는 것이 있으면 업데이트 하기 위함 */
-		Trade trade = tradeRepository.findByProductIdAndProductSizeAndTradeStatusAndPriceGreaterThanEqual(
+		/* product와 product size, 구매 입찰 중인 trade 조회 - 조회되는 것이 있으면 거래 체결 하기 위함 */
+		Optional<Trade> trade = tradeRepository.findByProductIdAndProductSizeAndTradeStatusAndPriceLessThanEqual(
+			product.getId(),
+			tradeRequest.getProductSize(),
+			TradeStatus.PURCHASE_PROGRESS, tradeRequest.getPrice());
+
+		/* 판매자가 찾는 구매 요청이 있을 경우 거래 체결 */
+		if (trade.isPresent()) {
+			trade.get().updateTradeByBuyer(tradeRequest.getUserId(), tradeRequest.getPrice());
+		} else {
+			/* 기존 요청한 판매 요청 있는지 조회 한다  */
+			trade = tradeRepository.findByPublisherIdAndProductIdAndProductSizeAndTradeStatus(seller.getId(),
 				product.getId(),
 				tradeRequest.getProductSize(),
-				TradeStatus.PURCHASE_PROGRESS,
-				tradeRequest.getPrice())
-			.orElse(tradeRepository.findByPublisherIdAndProductIdAndProductSizeAndTradeStatus(seller.getId(),
-					product.getId(),
-					tradeRequest.getProductSize(),
-					TradeStatus.PURCHASE_PROGRESS)
-				.orElse(tradeRequest.convertForSeller(product.getId())));
+				TradeStatus.SALE_PROGRESS);
 
-		/* 구매 입찰이 있는 경우 거래 체결을 위한 update, 기존 요청을 찾았을 경우 기존 요청 update  */
-		if (trade.getTradeStatus() == TradeStatus.PURCHASE_PROGRESS) {
-			trade.updateTradeBySeller(tradeRequest.getUserId(), tradeRequest.getPrice());
-		} else if (trade.getTradeStatus() == TradeStatus.SALE_PROGRESS) {
-			trade.updateTrade(tradeRequest.getPrice(), tradeRequest.getShippingAddress());
+			/* 기존 요청 내역 존재시 업데이트, 없으면 신규 생성 */
+			if (trade.isPresent()) {
+				trade.get().updateTrade(tradeRequest.getPrice(), tradeRequest.getShippingAddress());
+			} else {
+				trade = Optional.ofNullable(tradeRequest.convertForSeller(product.getId()));
+			}
 		}
 
 		/*
 		구매자에게 message 전송
 		 */
-		tradeRepository.save(trade);
+		trade.ifPresent(tradeRepository::save);
 	}
 }
