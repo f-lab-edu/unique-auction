545889fde98642c36cd41f3ef00ec62e4ff1dfe6
vymr1000 <nuseehr@naver.com>
2023-02-24 14:51:03 +0900

    feature: 운영환경 배포 스크립트, entrypoint, dockerfile, docker-comopose 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Blue 를 기준으로 현재 떠있는 컨테이너를 체크한다.
-EXIST_BLUE=$(docker-compose -p uniqueauction-blue -f /home/project/unique-auction/docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p uniqueauction-blue -f docker-compose.blue.yml ps | grep Up)
 
 # 컨테이너 스위칭
 if [ -z "$EXIST_BLUE" ]; then
   echo "blue up"
-  docker-compose -p uniqueauction-blue -f /home/project/unique-auction/docker-compose.blue.yml up -d
+  docker-compose -p uniqueauction-blue -f docker-compose.blue.yml up -d
   BEFORE_COMPOSE_COLOR="green"
   AFTER_COMPOSE_COLOR="blue"
 else
