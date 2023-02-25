01697ab607ac60035d984fb0cb8c38ed3e792127
vymr1000 <nuseehr@naver.com>
2023-02-26 01:01:12 +0900

    fix: docker deploy 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+# docker login & image pull
+docker pull vymr1000/unique-auction
+
 # Blue 를 기준으로 현재 떠있는 컨테이너를 체크한다.
-EXIST_BLUE=$(docker-compose -p unique-auction-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p unique-auction-blue -f /home/project/unique-auction/docker-compose.blue.yml ps | grep Up)
 
 # 컨테이너 스위칭
 if [ -z "$EXIST_BLUE" ]; then
   echo "blue up"
-  docker-compose -p unique-auction-blue -f docker-compose.blue.yml up -d
+  docker-compose -p unique-auction-blue -f /home/project/unique-auction/docker-compose.blue.yml up -d
   BEFORE_COMPOSE_COLOR="green"
   AFTER_COMPOSE_COLOR="blue"
 else
