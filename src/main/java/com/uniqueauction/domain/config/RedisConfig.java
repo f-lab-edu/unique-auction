24f5a00b267d8f294e1490fb8a4d0363be39e6f9
vymr1000 <nuseehr@naver.com>
2023-03-07 10:23:26 +0900

    fix: redis connection을 위한 config 수정

--- src/main/java/com/uniqueauction/domain/config/RedisConfig.java
+++ src/main/java/com/uniqueauction/domain/config/RedisConfig.java
@@ -3,7 +3,8 @@ package com.uniqueauction.domain.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -15,21 +16,24 @@ public class RedisConfig {
 
 	@Value("${spring.redis.host}")
 	private String host;
-
 	@Value("${spring.redis.port}")
 	private int port;
 
+	@Value("${spring.redis.password}")
+	private String password;
+
 	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+	public LettuceConnectionFactory lettuceConnectionFactory() {
+		final RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+		redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
 	}
 
 	@Bean
-	public RedisTemplate<String, Integer> redisTemplate() {
-		RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new StringRedisSerializer());
-		redisTemplate.setConnectionFactory(redisConnectionFactory());
-		return redisTemplate;
+	public RedisTemplate<String, Object> redisTemplate() {
+		final RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(lettuceConnectionFactory());
+		template.setDefaultSerializer(new StringRedisSerializer());
+		return template;
 	}
 }
