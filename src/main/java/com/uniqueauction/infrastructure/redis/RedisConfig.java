bc1728c494dfabba5eca724a7ffc6e574bc89e77
vymr1000 <nuseehr@naver.com>
2023-03-18 00:58:04 +0900

    fix: redisson client 설정을 위한 redisConfig 수정

--- src/main/java/com/uniqueauction/infrastructure/redis/RedisConfig.java
+++ src/main/java/com/uniqueauction/infrastructure/redis/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.uniqueauction.infrastructure.redis;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,5 +38,14 @@ public class RedisConfig {
 		template.setConnectionFactory(lettuceConnectionFactory());
 		template.setDefaultSerializer(new StringRedisSerializer());
 		return template;
+	}
+
+	@Bean
+	public RedissonClient redissonClient() {
+		Config config = new Config();
+		config.useSingleServer()
+			.setAddress("redis://" + host + ":" + port)
+			.setDatabase(0);
+		return Redisson.create(config);
 	}
 }
